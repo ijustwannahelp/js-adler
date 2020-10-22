74ba2587a630e7bab502281cb8e73baa7c7d6626
ijustwannahelp <nathangharris@protonmail.com>
2020-10-22 01:05:36 -0400

    Fixed implementation of Adler32

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 function hash(string) {
     var a = 1; b = 0;
     for(i = 0; i < string.length; i++) {
-        a = a+string.charCodeAt(i) % 65521;
+        a = a+string.charCodeAt(i);
         b = (b+a) % 65521;
     }
-    answer = (b << 16 | a).toString(16);
+    answer = (b << 16 | a >>> 0).toString(16);
     return answer;
 }
 module.exports.hash = hash;
